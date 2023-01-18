70186d8f9b56010a6dee9b9f505dac978d976db0
dguo153 <122913586+dguo153@users.noreply.github.com>
2023-01-17 20:00:21 -0800

    Update FileUtils.java

--- FileUtils.java
+++ FileUtils.java
@@ -1,1 +1,45 @@
+/**
+ * File Utilities for reading and writing.
+ * 
+ * @author Daphne Guo
+ * @since August 25, 2022
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+public class FileUtils {
+  
+  /**
+   * Opens a file to read using the Scanner class.
+   * @param fileName    name of the file to open
+   * @return            the Scanner object to open the file
+   */
+  public static java.util.Scanner openToRead(String fileName) {
+    java.util.Scanner input = null;
+    try {
+      input = new java.util.Scanner(new java.io.File(fileName));
+    }
+    catch(java.io.FileNotFoundException e) {
+      System.err.println("ERROR: Cannot open " + fileName + " for reading.");
+      System.exit(-1);
+    }
+    return input;
+  }
+  
+  /**
+   * Opens a file to write using the PrintWriter class.
+   * @param fileName    name of the file to open
+   * @return            the PrintWriter object to open the file
+   */
+  public static PrintWriter openToWrite(String fileName) {
+    PrintWriter output = null;
+    try {
+      output = new PrintWriter(new File(fileName));
+    }
+    catch(FileNotFoundException e) {
+      System.err.println("ERROR: Cannot open " + fileName + " for writing.");
+    }
+    return output;
+  }
+}
